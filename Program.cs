844169ced2ecec2a2b23c6dd090faa72e0b40ff1
naim90 <naim.elmoudni@gmail.com>
2021-09-17 16:28:51 +0200

    MonProgramme

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Challenge_ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string s = "abcdefghijklmnopqrstuvwxyz";
+            foreach (char c in s)
+            {
+                Console.WriteLine((int)c);
+            }
+
+
+
+        }
+    }
+}
